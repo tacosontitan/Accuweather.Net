372c585c1e446b54f7f4cb175d9bdb9180d026ce
tacosontitan <contact@tacosontitan.com>
2024-07-26 16:34:06 -0500

    Added summary file for errors class.

---
+++ src/Weatherstack/Errors/Summary.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2024 tacosontitan
+// This file is part of the Weatherstack project, which is distributed under the MIT license.
+// See LICENSE for more information.
+
+/*
+    The folder containing this file is not intended to be a part of the
+    namespace, it is purely for organizational purposes.
+*/
+
+namespace Weatherstack;
+
+/// <summary>
+///     Describes all predefined error responses that can be returned by the Weatherstack API.
+/// </summary>
+public static partial class Errors;
