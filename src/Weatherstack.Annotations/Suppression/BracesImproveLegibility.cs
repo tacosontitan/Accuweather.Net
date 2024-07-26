e91ec0ab9510f642d5b67657e433d046c7c42efc
tacosontitan <contact@tacosontitan.com>
2024-07-25 21:29:23 -0500

    Created annotations project.

---
+++ src/Weatherstack.Annotations/Suppression/BracesImproveLegibility.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2024 tacosontitan
+// This file is part of the Samsara.Net project, which is distributed under the MIT license.
+// See LICENSE for more information.
+
+namespace Weatherstack.Annotations.Suppression;
+
+public static partial class Justifications
+{
+    /// <summary>
+    /// Describes how braces can improve legibility.
+    /// </summary>
+    public const string BracesImproveLegibility =
+        """
+        Expression-bodied members are often easier to read for simple methods, but
+        for more complex methods, using braces can improve legibility and make the
+        code easier to understand. This can be especially helpful for developers who
+        are new to the codebase or who are not familiar with the language or framework.
+        """;
+}
---
+++ src/Weatherstack.Annotations/Suppression/GuardClausesShouldNotBeTerse.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2024 tacosontitan
+// This file is part of the Samsara.Net project, which is distributed under the MIT license.
+// See LICENSE for more information.
+
+namespace Weatherstack.Annotations.Suppression;
+
+public static partial class Justifications
+{
+    /// <summary>
+    /// Guard clauses should not be terse.
+    /// </summary>
+    public const string GuardClausesShouldNotBeTerse =
+        """
+        Condensing guard clauses with ternary expressions and similar methods can
+        result in a loss of clarity and can make it difficult for less experienced
+        developers to understand the intent of the code. This can lead to bugs and
+        other issues that are difficult to diagnose and resolve.
+        """;
+}
