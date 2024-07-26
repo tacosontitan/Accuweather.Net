3daf265ed2cbc64a6f7ff19822f16b1246b159ee
tacosontitan <contact@tacosontitan.com>
2024-07-25 21:39:04 -0500

    Added predefined errors.

---
+++ src/Weatherstack/Errors/TooManyRequests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2024 tacosontitan
+// This file is part of the Samsara.Net project, which is distributed under the MIT license.
+// See LICENSE for more information.
+
+/*
+    The folder containing this file is not intended to be a part of the
+    namespace, it is purely for organizational purposes.
+*/
+
+namespace Weatherstack;
+
+public static partial class Errors
+{
+    /// <summary>
+    /// User has reached his subscription's monthly request allowance.
+    /// </summary>
+    public static Error TooManyRequests => new()
+    {
+        Code = 429,
+        Type = "too_many_requests",
+        Message = "User has reached his subscription's monthly request allowance."
+    };
+}
---
+++ src/Weatherstack/Errors/Unauthorized.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2024 tacosontitan
+// This file is part of the Samsara.Net project, which is distributed under the MIT license.
+// See LICENSE for more information.
+
+/*
+    The folder containing this file is not intended to be a part of the
+    namespace, it is purely for organizational purposes.
+*/
+
+namespace Weatherstack;
+
+public static partial class Errors
+{
+    /// <summary>
+    /// User did not supply an access key / invalid access key.
+    /// </summary>
+    public static Error Unauthorized => new()
+    {
+        Code = 101,
+        Type = "unauthorized",
+        Message = "User did not supply an access key / invalid access key."
+    };
+}
