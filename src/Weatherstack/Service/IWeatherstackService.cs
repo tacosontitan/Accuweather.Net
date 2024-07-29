de975d2b177e17f91eb8b316524dd73394fe0160
tacosontitan <contact@tacosontitan.com>
2024-07-29 15:00:48 -0500

    Created the initial weatherstack service.

---
+++ src/Weatherstack/Service/IWeatherstackService.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2024 tacosontitan
+// This file is part of the Weatherstack project, which is distributed under the MIT license.
+// See LICENSE for more information.
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+
+/*
+    The folder containing this file is not intended to be a part of the
+    namespace, it is purely for organizational purposes.
+*/
+namespace Weatherstack;
+
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+/// <summary>
+///     Represents a service for interacting with the Weatherstack API.
+/// </summary>
+public interface IWeatherstackService
+{
+    /// <summary>
+    ///     Gets a <see cref="IServiceProvider"/> for resolving dependencies
+    ///     when interacting with the Weatherstack API.
+    /// </summary>
+    IServiceProvider ServiceProvider { get; }
+}
---
+++ src/Weatherstack/Service/WeatherstackService.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2024 tacosontitan
+// This file is part of the Weatherstack project, which is distributed under the MIT license.
+// See LICENSE for more information.
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+
+/*
+    The folder containing this file is not intended to be a part of the
+    namespace, it is purely for organizational purposes.
+*/
+namespace Weatherstack;
+
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+/// <summary>
+///     Represents a service for interacting with the Weatherstack API.
+/// </summary>
+/// <param name="serviceProvider">
+///     A service provider for resolving dependencies
+///     when interacting with the Weatherstack API.
+/// </param>
+public sealed class WeatherstackService(IServiceProvider serviceProvider)
+    : IWeatherstackService
+{
+    /// <inheritdoc />
+    public IServiceProvider ServiceProvider => serviceProvider;
+}
+
