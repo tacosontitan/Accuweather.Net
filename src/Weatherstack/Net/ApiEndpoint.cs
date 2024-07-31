c77c95c3b44c2e934b2272a93e47bbf21284cadc
tacosontitan <contact@tacosontitan.com>
2024-07-30 19:53:45 -0500

    Created an API endpoint model for encapsulation of common definitions.

---
+++ src/Weatherstack/Net/ApiEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+
+namespace Weatherstack.Net;
+
+/// <summary>
+///     Represents an endpoint for the Weatherstack API.
+/// </summary>
+/// <param name="method">
+///     The HTTP method to use for the request.
+/// </param>
+/// <param name="url">
+///     The URL to use for the request.
+/// </param>
+internal abstract class ApiEndpoint(
+    HttpMethod method,
+    string url)
+{
+    /// <summary>
+    ///     Specifies the HTTP method to use for the request.
+    /// </summary>
+    public HttpMethod Method { get; } = method;
+
+    /// <summary>
+    ///     Specifies the URL to use for the request.
+    /// </summary>
+    public string Url { get; } = url;
+}
