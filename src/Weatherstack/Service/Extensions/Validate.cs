d91138bb1f9d732c498946df37c365b4c082104c
tacosontitan <contact@tacosontitan.com>
2024-07-30 19:54:06 -0500

    Defined validation extension for the weatherstack service.

---
+++ src/Weatherstack/Service/Extensions/Validate.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2024 tacosontitan
+// This file is part of the Weatherstack project, which is distributed under the MIT license.
+// See LICENSE for more information.
+
+/*
+    The folder containing this file is not intended to be a part of the
+    namespace, it is purely for organizational purposes.
+*/
+namespace Weatherstack;
+
+/// <summary>
+///     Defines extension methods for the <see cref="IWeatherstackService"/>
+///     interface to simplify interactions with the Weatherstack API.
+/// </summary>
+public static partial class WeatherstackServiceExtensions
+{
+    private static void Validate(this IWeatherstackService source)
+    {
+        AssertNotNull(source);
+        ValidateServiceProvider(source);
+    }
+
+    private static void AssertNotNull(IWeatherstackService service)
+    {
+        #if NET6_0_OR_GREATER
+
+        ArgumentNullException.ThrowIfNull(service);
+
+        #else
+
+        if (service is null)
+            throw new ArgumentNullException(nameof(service));
+
+        #endif
+    }
+
+    private static void ValidateServiceProvider(IWeatherstackService source)
+    {
+        const string errorMessage = "The service provider cannot be null.";
+
+        #if NET6_0_OR_GREATER
+
+        InvalidOperationException.ThrowIfNull(source.ServiceProvider, errorMessage);
+
+        #else
+
+        if (source.ServiceProvider is null)
+            throw new InvalidOperationException(errorMessage);
+
+        #endif
+    }
+}
